68dd2e55a49bd729425342c38eca4215902c53cb
Antoine Pirrone <apirrone@emi.u-bordeaux1.fr>
2015-02-03 09:28:55 +0100

    deleteGrid() copyGrid()

--- src/grid.c
+++ src/grid.c
@@ -1,7 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "grid.h"
 #include <time.h>
+#include <assert.h>
+#include "grid.h"
 
 struct grid_s{
 	tile tab[GRID_SIDE][GRID_SIDE];
@@ -12,13 +13,13 @@ static int rand(int a, int b){
 	return rand()%(b-a) +a;
 }
 
-static void generateTile(){
+static void generateTile(grid g){
 	int i = -1;
 	int j = -1;
 	while(i == -1 && j == -1){
 		i = rand(0, GRID_SIDE);
 		j = rand(0, GRID_SIDE);
-		if(tab[i][j] != 0){
+		if(g->tab[i][j] != 0){
 			i = -1;
 			j = -1;
 		}
@@ -28,24 +29,35 @@ static void generateTile(){
 	int nb = rand(0, 6);
 	switch(nb){
 		case 5 : 
-			tab[i][j] = 2;
+			g->tab[i][j] = 2;
 			break;
 
 		default : 
-			tab[i][j] = 1;
+			g->tab[i][j] = 1;
 	}
 }
 
 grid new grid(){
 
+	grid g = malloc(sizeof(*g));
+	assert(g != null);
 	for (int i = 0; i < GRID_SIDE; i++)
 		for (int j = 0; j < GRID_SIDE; j++)
-			tab[i][j] = 0;
+			g->tab[i][j] = 0;
 
 
-	generateTile();
-	generateTile();
+	generateTile(g);
+	generateTile(g);
 
+}
 
+void deleteGrid(grid g){
+	free g;
+}
+
+void copy_grid(grid src, grid dst){
+	for (int i = 0; i < GRID_SIDE; i++)
+		for (int j = 0; j < GRID_SIDE; j++)
+			dst->tab[i][j] = grid[i][j];
 }
 
